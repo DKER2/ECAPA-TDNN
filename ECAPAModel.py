5e80b25d167ed0ea171885a727774397acba47ba
DKER2 <pdanghuy03@gmail.com>
2023-04-02 21:32:09 +0800

    Add Phone Loss

--- ECAPAModel.py
+++ ECAPAModel.py
@@ -33,7 +33,7 @@ class ECAPAModel(nn.Module):
 			speaker_embedding, phonemes, seq_len = self.speaker_encoder.forward(data.cuda(), seq_len, aug = True)
 			nloss, prec       = self.speaker_loss.forward(speaker_embedding, labels)
 			loss_phn = self.phoneme_loss.forward(phonemes, seq_len)
-			if epoch==1:
+			if epoch<=2:
 				loss = loss_phn
 			else:
 				loss = 0.1*loss_phn + 0.9*nloss
