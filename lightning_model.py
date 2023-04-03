8af10a6091f04a2cc1eb0316e6b07d3e13c24226
DKER2 <pdanghuy03@gmail.com>
2023-04-03 16:19:44 +0800

    Add loss

--- lightning_model.py
+++ lightning_model.py
@@ -18,6 +18,9 @@ class Task(LightningModule):
 
         self.optim           = torch.optim.Adam(self.parameters(), lr = lr, weight_decay = 2e-5)
 		self.scheduler       = torch.optim.lr_scheduler.StepLR(self.optim, step_size = test_step, gamma=lr_decay)
+
+        self.eval_list=kwargs['eval_list']
+        self.eval_path=kwargs['eval_path']
     
     def training_step(self, batch, batch_idx):
         data, seq_len, labels = batch
@@ -30,29 +33,63 @@ class Task(LightningModule):
         self.log('acc', prec, prog_bar=True)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        audio, seq_len, labels = batch
-        data_1 = torch.FloatTensor(numpy.stack([audio],axis=0))
+    def on_training_epoch_end(self):
+        eval_network(self.eval_list, self.eval_path)
 
-        # Spliited utterance matrix
-        max_audio = 300 * 160 + 240
-        if audio.shape[0] <= max_audio:
-            shortage = max_audio - audio.shape[0]
-            audio = numpy.pad(audio, (0, shortage), 'wrap')
-        feats = []
-        startframe = numpy.linspace(0, audio.shape[0]-max_audio, num=5)
-        for asf in startframe:
-            feats.append(audio[int(asf):int(asf)+max_audio])
-        feats = numpy.stack(feats, axis = 0).astype(numpy.float)
-        data_2 = torch.FloatTensor(feats).cuda()
-        # Speaker embeddings
-        with torch.no_grad():
-            embedding_1, pho_out, seg_len = self.speaker_encoder.forward(data_1, aug = False)
-            embedding_1 = F.normalize(embedding_1, p=2, dim=1)
-            embedding_2, pho_out, seg_len = self.speaker_encoder.forward(data_2, aug = False)
-            embedding_2 = F.normalize(embedding_2, p=2, dim=1)
-        embeddings[file] = [embedding_1, embedding_2]
+    def eval_network(self, eval_list, eval_path):
+		self.speaker_encoder.eval()
+		files = []
+		embeddings = {}
+		lines = open(eval_list).read().splitlines()
+		for line in lines:
+			files.append(line.split()[1])
+			files.append(line.split()[2])
+		setfiles = list(set(files))
+		setfiles.sort()
+
+		for idx, file in tqdm.tqdm(enumerate(setfiles), total = len(setfiles)):
+			audio, _  = soundfile.read(os.path.join(eval_path, file))
+			# Full utterance
+			data_1 = torch.FloatTensor(numpy.stack([audio],axis=0)).cuda()
+
+			# Spliited utterance matrix
+			max_audio = 300 * 160 + 240
+			if audio.shape[0] <= max_audio:
+				shortage = max_audio - audio.shape[0]
+				audio = numpy.pad(audio, (0, shortage), 'wrap')
+			feats = []
+			startframe = numpy.linspace(0, audio.shape[0]-max_audio, num=5)
+			for asf in startframe:
+				feats.append(audio[int(asf):int(asf)+max_audio])
+			feats = numpy.stack(feats, axis = 0).astype(numpy.float)
+			data_2 = torch.FloatTensor(feats).cuda()
+			# Speaker embeddings
+			with torch.no_grad():
+				embedding_1, pho_out, seg_len = self.speaker_encoder.forward(data_1, [data_1.shape[1]], aug = False)
+				embedding_1 = F.normalize(embedding_1, p=2, dim=1)
+				embedding_2, pho_out, seg_len = self.speaker_encoder.forward(data_2, [data_2.shape[1] for i in range(data_2.shape[0])], aug = False)
+				embedding_2 = F.normalize(embedding_2, p=2, dim=1)
+			embeddings[file] = [embedding_1, embedding_2]
 		scores, labels  = [], []
+
+		for line in lines:			
+			embedding_11, embedding_12 = embeddings[line.split()[1]]
+			embedding_21, embedding_22 = embeddings[line.split()[2]]
+			# Compute the scores
+			score_1 = torch.mean(torch.matmul(embedding_11, embedding_21.T)) # higher is positive
+			score_2 = torch.mean(torch.matmul(embedding_12, embedding_22.T))
+			score = (score_1 + score_2) / 2
+			score = score.detach().cpu().numpy()
+			scores.append(score)
+			labels.append(int(line.split()[0]))
+			
+		# Coumpute EER and minDCF
+		EER = tuneThresholdfromScore(scores, labels, [1, 0.1])[1]
+		fnrs, fprs, thresholds = ComputeErrorRates(scores, labels)
+		minDCF, _ = ComputeMinDcf(fnrs, fprs, thresholds, 0.05, 1, 1)
+        print("\nValidation error : ", EER)
+        print("Validation minDCF : ", minDCF)
+		return EER, minDCF
 
     def configure_optimizers(self):
         return [self.optim], [self.scheduler]
---
+++ train.py
@@ -0,0 +1,69 @@
+import argparse, glob, os, torch, warnings, time
+from tools import *
+from dataLoader import train_loader
+from pytorch_lightning.plugins import DDPPlugin
+from lightning_model import Task
+
+parser = argparse.ArgumentParser(description = "ECAPA_trainer")
+## Training Settings
+parser.add_argument('--num_frames', type=int,   default=200,     help='Duration of the input segments, eg: 200 for 2 second')
+parser.add_argument('--max_epoch',  type=int,   default=80,      help='Maximum number of epochs')
+parser.add_argument('--batch_size', type=int,   default=400,     help='Batch size')
+parser.add_argument('--n_cpu',      type=int,   default=4,       help='Number of loader threads')
+parser.add_argument('--test_step',  type=int,   default=1,       help='Test and save every [test_step] epochs')
+parser.add_argument('--lr',         type=float, default=0.001,   help='Learning rate')
+parser.add_argument("--lr_decay",   type=float, default=0.97,    help='Learning rate decay every [test_step] epochs')
+
+## Training and evaluation path/lists, save path
+parser.add_argument('--train_list', type=str,   default="/data08/VoxCeleb2/train_list.txt",     help='The path of the training list, https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/train_list.txt')
+parser.add_argument('--train_path', type=str,   default="/data08/VoxCeleb2/train/wav",                    help='The path of the training data, eg:"/data08/VoxCeleb2/train/wav" in my case')
+parser.add_argument('--eval_list',  type=str,   default="/data08/VoxCeleb1/veri_test2.txt",              help='The path of the evaluation list, veri_test2.txt comes from https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt')
+parser.add_argument('--eval_path',  type=str,   default="/data08/VoxCeleb1/test/wav",                    help='The path of the evaluation data, eg:"/data08/VoxCeleb1/test/wav" in my case')
+parser.add_argument('--musan_path', type=str,   default="/data08/Others/musan_split",                    help='The path to the MUSAN set, eg:"/data08/Others/musan_split" in my case')
+parser.add_argument('--rir_path',   type=str,   default="/data08/Others/RIRS_NOISES/simulated_rirs",     help='The path to the RIR set, eg:"/data08/Others/RIRS_NOISES/simulated_rirs" in my case');
+parser.add_argument('--save_path',  type=str,   default="exps/exp1",                                     help='Path to save the score.txt and models')
+parser.add_argument('--initial_model',  type=str,   default="",                                          help='Path of the initial_model')
+
+## Model and Loss settings
+parser.add_argument('--C',       type=int,   default=1024,   help='Channel size for the speaker encoder')
+parser.add_argument('--m',       type=float, default=0.2,    help='Loss margin in AAM softmax')
+parser.add_argument('--s',       type=float, default=30,     help='Loss scale in AAM softmax')
+parser.add_argument('--n_class', type=int,   default=5994,   help='Number of speakers')
+
+## Command
+parser.add_argument('--eval',    dest='eval', action='store_true', help='Only do evaluation')
+
+## Initialization
+warnings.simplefilter("ignore")
+torch.multiprocessing.set_sharing_strategy('file_system')
+args = parser.parse_args()
+args = init_args(args)
+
+
+trainloader = train_loader(**vars(args))
+trainLoader = torch.utils.data.DataLoader(trainloader, batch_size = args.batch_size, shuffle = True, num_workers = args.n_cpu, drop_last = True)
+Task(**args.__dict__)
+
+if args.initial_model is not None:
+        state_dict = torch.load(args.initial_model, map_location="cpu")["state_dict"]
+        model.load_state_dict(state_dict, strict=True)
+        print("load weight from {}".format(args.initial_model))
+
+checkpoint_callback = ModelCheckpoint(monitor='cosine_eer', save_top_k=100,
+           filename="{epoch}_{cosine_eer:.2f}", dirpath=args.save_dir)
+lr_monitor = LearningRateMonitor(logging_interval='step')
+
+AVAIL_GPUS = torch.cuda.device_count()
+trainer = Trainer(
+        max_epochs=args.max_epochs,
+        plugins=DDPPlugin(find_unused_parameters=False),
+        gpus=-1,
+        num_sanity_val_steps=0,
+        sync_batchnorm=True,
+        callbacks=[checkpoint_callback, lr_monitor],
+        default_root_dir=args.save_dir,
+        reload_dataloaders_every_n_epochs=1,
+        accumulate_grad_batches=1,
+        log_every_n_steps=25,
+        )
+trainer.fit(model, train_dataloaders=trainLoader)
